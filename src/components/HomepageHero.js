6c5fa4e44d39ea2443a4cdaff017e9c58b092e3d
Symbiotic Love <matthew@symbiotic.love>
2023-07-26 22:19:22 -0700

    Update

--- src/components/HomepageHero.js
+++ src/components/HomepageHero.js
@@ -5,7 +5,7 @@ const HomepageHero = () => {
     return (
         <section className="homepage-hero">
             <img src="https://placehold.co/400" className="author-image" />
-            <h2 className="hero-certifications-title">Hey There!</h2>
+            <h2 className="hero-certifications-title">Hey there. I'm glad you're here!</h2>
             <p className="hero-description">
                 I am a utility full-stack engineer, forged through unconventional training, with the adaptability and competence to work across development teams. My journey in the engineering field is characterized by practical application and production-quality work spanning the entire development process. While my prowess is prominently showcased in areas such as React, Python, and API creation and usage, my capabilities extend far beyond. With approximately 2000 hours of hands-on experience, I have developed job-ready proficiency in a wide array of disciplines that empowers me to conquer a diverse range of technical challenges.
             </p>
