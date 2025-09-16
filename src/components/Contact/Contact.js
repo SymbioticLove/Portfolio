f66dbbfd3c879b76c7f3c35775651c0d674d27b7
Symbiotic Love <matthew@symbiotic.love>
2025-09-16 03:31:20 -0700

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,2 +1,49 @@
+import React from 'react';
+import './Contact.css';
+import { useSelector } from 'react-redux';
+
+const Contact = () => {
+  const contactData = useSelector(state => state.about.contact);
+
+  const {
+    contactTitle,
+    introduction,
+    collaboration,
+    emailButtonLabel,
+    emailButtonHref,
+    callButtonLabel,
+    callButtonHref,
+    closing,
+    signature,
+  } = contactData;
+
+  return (
+    <div className="contact">
+      {/* Contact Me title */}
+      <h1>{contactTitle}</h1>
+
+      {/* Introduction paragraph */}
+      <p>{introduction}</p>
+
+      {/* Collaboration paragraph */}
+      <p>{collaboration}</p>
+
+      {/* Contact buttons */}
+      <div className="contact-buttons">
+        {/* Email button */}
+        <a href={emailButtonHref} className="email-button">
+          {emailButtonLabel}
+        </a>
+
+       
+
+      {/* Closing paragraph */}
+      <p>{closing}</p>
+
+      {/* Signature */}
+      <div className="signature2">{signature}</div>
+    </div>
+  );
+};
 
 
